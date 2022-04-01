28b4478a6e6d79eaf3002ca7cb5718c442a52010
srdante <minidantebtc@gmail.com>
2022-04-01 02:12:36 -0300

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cryptounifier',
-    version='1.0.1',
+    version='1.0.2',
     description='CryptoUnifier API Python Integration.',
     author='https://cryptounifier.io/',
     license='MIT License',
