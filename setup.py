b7f4141ce153baf1ee6d3fdd419785a387639add
srdante <minidantebtc@gmail.com>
2022-04-01 02:08:02 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cryptounifier',
-    version='1.0.0',
+    version='1.0.1',
     description='CryptoUnifier API Python Integration.',
     url='https://cryptounifier.io/',
     author='https://cryptounifier.io/',
