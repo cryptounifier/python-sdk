c2629f6f86d44f724699e772a54b51df822f9feb
srdante <35383529+srdante@users.noreply.github.com>
2023-03-12 01:10:45 -0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='cryptounifier',
-    version='1.0.2',
+    version='1.0.3',
     description='CryptoUnifier API Python Integration.',
     author='https://cryptounifier.io/',
     license='MIT License',
